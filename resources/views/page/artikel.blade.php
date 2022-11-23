43d874b4648011c47cf5c80d419a4f47619d6f6b
Agustiawan <44895587+thype5209@users.noreply.github.com>
2022-11-24 00:18:41 +0800

    Ganti Design Artikel

--- resources/views/page/artikel.blade.php
+++ resources/views/page/artikel.blade.php
@@ -1,20 +1,82 @@
 @extends('page.layout')
 @section('content')
-<div class="container mx-auto px-4 py-2" style="background-color:whitesmoke">
-    <h5 class="prose px-3 py-1 text-lg font-semibold">1. Bahan</h5>
-    <article class="prose text-justify text-base">Salah satu modal yang harus dipersiapkan dalam usaha minuman boba adalah barang habis jadi. Termasuk di dalam barang habis jadi ini adalah bahan pembuat boba dan minumannya. Bahan-bahan yang dibutuhkan untuk membuat boba sendiri antara lain tepung tapioka, air, serta teh hitam untuk pewarnanya dan juga penambah cita rasa boba yang kuat. Agar bisa memperoleh harga yang terjangkau, calon penjual bisa membeli bahan-bahan tersebut di online shop.
-    Menurut salah satu situs e-commerce, tepung tapioka dapat dibeli dengan harga sekitar 140 ribu rupiah tiap 10 kg sedangkan teh hitamnya sekitar 115 ribu rupiah. Ada pula bahan tambahan seperti gula aren dan susu kental manis yang dapat dibeli dengan harga sekitar 200 ribu rupiah. Artinya uang yang dibutuhkan untuk membeli bahan pembuatan minuman boba sekitar 445 ribu rupiah.
-    </article>
-    <h5 class="prose px-3 py-1 text-lg font-semibold">2. Peralatan</h5>
-    <article class="prose text-justify text-base">Barang habis jadi yang juga perlu dipersiapkan adalah alat-alat. Peralatan yang dibutuhkan untuk membuka usaha minuman boba antara lain adalah coolbox, gelas plastik, sedotan boba, dan cup sealer. Berdasarkan salah satu e-commerce, 100 pcs gelas plastik dapat dibeli dengan harga Rp34.000 dan 100 sedotan boba seharga Rp11.000.
-    Untuk cool box sebenarnya adalah peralatan yang opsional. Jika sudah memiliki kulkas, penjual tidak perlu membeli coolbox. Namun jika ingin, coolbox dapat dibeli dengan harga sekitar Rp 120 ribu. Sedangkan harga cup sealer berkisar antara Rp 600 ribuan. Jadi, modal usaha minuman boba yang digunakan dalam membeli peralatan penjualannya adalah sekitar Rp765.000.
-    Selain itu, sebenarnya juga dibutuhkan beberapa alat-alat dasar untuk pembuatan boba. Alat-alat tersebut antara lain saringan, ketel atau teko kecil, panci dan lain sebagainya. Namun alat-alat itu bisa didapatkan di dapur rumah tanpa perlu membelinya lagi.</article>
-    <h5 class="prose px-3 py-1 text-lg font-semibold">3. Gerobak Atau Booth</h5>
-    <article class="prose text-justify text-base">Jika usaha penjualan minuman boba akan dibuka di pinggir jalan, maka dibutuhkan gerobak atau booth sebagai tempat jualannya. Booth minuman boba dapat dibeli dengan harga yang murah melalui online shop, yaitu sekitar 590 ribu rupiah.</article>
-    <h5 class="prose px-3 py-1 text-lg font-semibold">4. Sewa Tempat</h5>
-    <article class="prose text-justify text-base">Jika berencana membuka usaha minuman boba di tempat yang tetap, maka disarankan untuk menyewa tempat jualan. Harga sewa tempat sendiri bervariasi, tergantung lokasi yang dipilih. Namun  pada umumnya, penyewaan tempat jualan minuman boba berkisar Rp 1 jutaan per bulan.</article>
-    <h5 class="prose px-3 py-1 text-lg font-semibold">5. Gaji Karyawan</h5>
-    <article class="prose text-justify text-base">Beberapa pendiri bisnis mungkin tidak bisa menjalankan bisnisnya sendiri karena alasan tertentu. Oleh karena itu, ada kalanya diperlukan seorang karyawan untuk menjaga booth atau tempat penjualan minuman boba. Biaya yang perlu disiapkan untuk menggaji seorang pegawai sebenarnya bisa bervariasi sesuai dengan kebijakan yang berlaku. Namun pada umumnya, karyawan untuk penjualan minuman boba per bulannya digaji sekitar Rp.1.200.000.
-    Itulah tadi paparan mengenai modal usaha minuman boba. Menurut paparan tersebut, dapat disimpulkan bahwa total modal yang dibutuhkan untuk membuka usaha minuman boba adalah sekitar Rp4.000.000. Sebenarnya ada banyak sekali pilihan dalam membuka suatu usaha bisnis. Namun, lagi-lagi setiap usaha memiliki tantangan-tantangannya sendiri yang tentunya tidak mudah. Jadi, tetap semangat dan jangan menyerah.</article>
-</div>
+    <div class="container mx-auto px-4 py-2" style="background-color:whitesmoke">
+        <div class="grid grid-cols-2 gap-4 box-border">
+            <div class="card !px-2 !py-2 bg-info text-info-content">
+                <div class="card-body">
+                    <h5 class="card-title">1. Bahan</h5>
+                    <article class="prose text-justify text-base">Salah satu modal yang harus dipersiapkan dalam usaha
+                        minuman
+                        boba adalah barang habis jadi. Termasuk di dalam barang habis jadi ini adalah bahan pembuat boba dan
+                        minumannya. Bahan-bahan yang dibutuhkan untuk membuat boba sendiri antara lain tepung tapioka, air,
+                        serta teh hitam untuk pewarnanya dan juga penambah cita rasa boba yang kuat. Agar bisa memperoleh
+                        harga
+                        yang terjangkau, calon penjual bisa membeli bahan-bahan tersebut di online shop.
+                        Menurut salah satu situs e-commerce, tepung tapioka dapat dibeli dengan harga sekitar 140 ribu
+                        rupiah
+                        tiap 10 kg sedangkan teh hitamnya sekitar 115 ribu rupiah. Ada pula bahan tambahan seperti gula aren
+                        dan
+                        susu kental manis yang dapat dibeli dengan harga sekitar 200 ribu rupiah. Artinya uang yang
+                        dibutuhkan
+                        untuk membeli bahan pembuatan minuman boba sekitar 445 ribu rupiah.
+                    </article>
+                </div>
+            </div>
+            <div class="card !px-2 !py-2 bg-info text-info-content">
+                <div class="card-body">
+                    <h5 class="prose px-3 py-1 text-lg font-semibold">2. Peralatan</h5>
+                    <article class="prose text-justify text-base">Barang habis jadi yang juga perlu dipersiapkan adalah
+                        alat-alat.
+                        Peralatan yang dibutuhkan untuk membuka usaha minuman boba antara lain adalah coolbox, gelas
+                        plastik, sedotan
+                        boba, dan cup sealer. Berdasarkan salah satu e-commerce, 100 pcs gelas plastik dapat dibeli dengan
+                        harga
+                        Rp34.000 dan 100 sedotan boba seharga Rp11.000.
+                        Untuk cool box sebenarnya adalah peralatan yang opsional. Jika sudah memiliki kulkas, penjual tidak
+                        perlu
+                        membeli coolbox. Namun jika ingin, coolbox dapat dibeli dengan harga sekitar Rp 120 ribu. Sedangkan
+                        harga cup
+                        sealer berkisar antara Rp 600 ribuan. Jadi, modal usaha minuman boba yang digunakan dalam membeli
+                        peralatan
+                        penjualannya adalah sekitar Rp765.000.
+                        Selain itu, sebenarnya juga dibutuhkan beberapa alat-alat dasar untuk pembuatan boba. Alat-alat
+                        tersebut antara
+                        lain saringan, ketel atau teko kecil, panci dan lain sebagainya. Namun alat-alat itu bisa didapatkan
+                        di dapur
+                        rumah tanpa perlu membelinya lagi.</article>
+                </div>
+            </div>
+            <div class="card !px-2 !py-2 bg-info text-info-content">
+                <div class="card-body"><h5 class="prose px-3 py-1 text-lg font-semibold">3. Gerobak Atau Booth</h5>
+                    <article class="prose text-justify text-base">Jika usaha penjualan minuman boba akan dibuka di pinggir jalan, maka
+                        dibutuhkan gerobak atau booth sebagai tempat jualannya. Booth minuman boba dapat dibeli dengan harga yang murah
+                        melalui online shop, yaitu sekitar 590 ribu rupiah.</article></div>
+            </div>
+            <div class="card !px-2 !py-2 bg-info text-info-content">
+                <div class="card-body">
+                    <h5 class="prose px-3 py-1 text-lg font-semibold">4. Sewa Tempat</h5>
+                    <article class="prose text-justify text-base">Jika berencana membuka usaha minuman boba di tempat yang tetap, maka
+                        disarankan untuk menyewa tempat jualan. Harga sewa tempat sendiri bervariasi, tergantung lokasi yang dipilih.
+                        Namun pada umumnya, penyewaan tempat jualan minuman boba berkisar Rp 1 jutaan per bulan.</article>
+                </div>
+            </div>
+            <div class="card !px-2 !py-2 bg-info text-info-content">
+                <div class="card-body">
+                    <h5 class="prose px-3 py-1 text-lg font-semibold">5. Gaji Karyawan</h5>
+                    <article class="prose text-justify text-base">Beberapa pendiri bisnis mungkin tidak bisa menjalankan bisnisnya
+                        sendiri karena alasan tertentu. Oleh karena itu, ada kalanya diperlukan seorang karyawan untuk menjaga booth
+                        atau tempat penjualan minuman boba. Biaya yang perlu disiapkan untuk menggaji seorang pegawai sebenarnya bisa
+                        bervariasi sesuai dengan kebijakan yang berlaku. Namun pada umumnya, karyawan untuk penjualan minuman boba per
+                        bulannya digaji sekitar Rp.1.200.000.
+                        Itulah tadi paparan mengenai modal usaha minuman boba. Menurut paparan tersebut, dapat disimpulkan bahwa total
+                        modal yang dibutuhkan untuk membuka usaha minuman boba adalah sekitar Rp4.000.000. Sebenarnya ada banyak sekali
+                        pilihan dalam membuka suatu usaha bisnis. Namun, lagi-lagi setiap usaha memiliki tantangan-tantangannya sendiri
+                        yang tentunya tidak mudah. Jadi, tetap semangat dan jangan menyerah.</article>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
 @endsection
