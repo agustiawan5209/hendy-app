d89855d85dad8292d93a4ab923d81b24734c40c6
Agustiawan <44895587+thype5209@users.noreply.github.com>
2022-12-01 23:02:06 +0800

    Update MyChartJs

--- resources/js/mychart.js
+++ resources/js/mychart.js
@@ -1,29 +1,6 @@
 import Chart from 'chart.js/auto';
 
-function GetTableKriteria() {
-    var Kriteria;
-    $.ajax({
-        type: "GET",
-        url: "/NilaiBobotKriteria/GetKriteria",
-        async: false,
-        success: function (response) {
-            Kriteria = response['kriteria'];
-        }
-    });
-    return Kriteria
-}
 
-function colorize(opaque) {
-    return (ctx) => {
-        const v = ctx.parsed.y;
-        const c = v < -50 ? '#D60000' :
-            v < 0 ? '#F46300' :
-            v < 50 ? '#0358B6' :
-            '#44DE28';
-
-        return opaque ? c : Utils.transparentize(c, 1 - Math.abs(v / 150));
-    };
-}
 var url = '/NilaiBobotAlternatif/NilaiAKhir/Hasil';
 var chart = $("#myChart");
 if (chart.length > 0) {
@@ -31,10 +8,8 @@ if (chart.length > 0) {
         type: "GET",
         url: url,
         success: function (response) {
-            var nama = new Array()
-            var ranking = new Array()
-            var table = '';
-            let kriteria = GetTableKriteria();
+            var nama = new Array();
+            var ranking = new Array();
             // console.log(response);
             // console.log(response.length);
             response.forEach((element, index, array) => {
