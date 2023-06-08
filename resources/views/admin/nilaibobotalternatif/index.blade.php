3142af4b1ea222c86712120905dc9e27ab25e565
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-06-09 01:41:03 +0800

    cek data alternatif

--- resources/views/admin/nilaibobotalternatif/index.blade.php
+++ resources/views/admin/nilaibobotalternatif/index.blade.php
@@ -101,20 +101,20 @@
                                     $nilai1 = \App\Http\Controllers\NilaiBobotAlternatifController::NilaiBobotAlternatif($kode_kriteria, $alternatif[$baris]['kode'], $alternatif[$kolom]['kode']);
                                     $nilai2 = \App\Http\Controllers\NilaiBobotAlternatifController::NilaiBobotAlternatif2($kode_kriteria, $alternatif[$baris]['kode'], $alternatif[$kolom]['kode']);
                                 @endphp
-                                <td>{{ round($nilai1 / $nilai2,3) }}</td>
+                                <td>{{ $nilai1 .'/'. $nilai2 }}</td>
                             @else
                                 @if ($baris > $kolom)
                                     @php
                                         $nilai1 = \App\Http\Controllers\NilaiBobotAlternatifController::NilaiBobotAlternatif($kode_kriteria, $alternatif[$baris]['kode'], $alternatif[$kolom]['kode']);
                                         $nilai2 = \App\Http\Controllers\NilaiBobotAlternatifController::NilaiBobotAlternatif2($kode_kriteria, $alternatif[$baris]['kode'], $alternatif[$kolom]['kode']);
                                     @endphp
-                                    <td>{{ round($nilai1 / $nilai2,3) }}</td>
+                                    <td>{{ $nilai1 .'/'. $nilai2 }}</td>
                                 @elseif($kolom > $baris)
                                     @php
                                         $nilai1 = \App\Http\Controllers\NilaiBobotAlternatifController::NilaiBobotAlternatif($kode_kriteria, $alternatif[$baris]['kode'], $alternatif[$kolom]['kode']);
                                         $nilai2 = \App\Http\Controllers\NilaiBobotAlternatifController::NilaiBobotAlternatif2($kode_kriteria, $alternatif[$baris]['kode'], $alternatif[$kolom]['kode']);
                                     @endphp
-                                    <td>{{ round($nilai1 / $nilai2,3) }}</td>
+                                    <td>{{ $nilai1 .'/'. $nilai2 }}</td>
                                 @endif
                             @endif
                         @endfor
